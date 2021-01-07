14d5d22e2bff184206eee5da742110fbe981862e
pulivarthi satya <54475209+Satya1804@users.noreply.github.com>
2021-01-07 12:05:26 +0530

    Add files via upload

---
+++ Currency.java
@@ -0,0 +1,70 @@
+//EXPERIMENT-12
+
+package pack;
+import java.util.Scanner;
+
+public class Currency{
+	public static void main(String[] args){
+	
+		Xcurrency c= new Xcurrency();
+		c.dollarToRupee();
+		c.euroToRupee();
+		c.yenToRupee();
+		c.rupeeToDollar();
+		c.rupeeToEuro();
+		c.rupeeToYen();
+
+	}
+}
+
+class Xcurrency{
+		Scanner sc = new Scanner(System.in);
+		double INR, USD , EURO , YEN ;
+
+		public void dollarToRupee(){
+			System.out.println("Enter how many dollars to convert into Rupees : ");
+			USD = sc.nextDouble();
+			INR = USD*73 ;
+			System.out.println("Dollar ="+USD+"equal to INR="+INR);
+		} 
+		
+		public void euroToRupee(){
+			System.out.println("Enter how many euros to convert into Rupees : ");
+			EURO = sc.nextDouble();
+			INR = EURO*90 ;
+			System.out.println(EURO +"EURO'S Equals to "+INR+"INR");
+		}
+				
+		public void yenToRupee(){
+			System.out.println("Enter how many YEN'S to convert into Rupees : ");
+			YEN = sc.nextDouble();
+			INR = YEN*0.71;
+			System.out.println(YEN +"YEN'S Equals to "+INR+"INR");
+		}
+
+		public void rupeeToDollar(){
+			System.out.println("Enter how many Rupees to convert into dollars : ");
+			INR = sc.nextDouble();
+			USD = INR/73 ;
+			System.out.println("INR ="+INR+"equal to Dollar="+USD);
+		} 
+
+		public void rupeeToEuro(){
+			System.out.println("Enter Rupees to convert into Euro:");
+			INR = sc.nextDouble();
+			EURO =(INR/90);
+			System.out.println("Rupee ="+INR +"equal to Euro="+EURO);
+		} 
+
+		public void rupeeToYen(){
+			System.out.println("Enter Rupees to convert into Yen:");
+			INR = sc.nextDouble();
+			YEN =(INR/0.71);
+			System.out.println("INR="+INR +"equal to YEN"+YEN);
+		}	
+	
+}
+
+
+
+
---
+++ ExptHandling.java
@@ -0,0 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class ExptHandling
+{
+   public static void main(String args[])
+   {
+      Scanner sc= new Scanner(System.in);
+      try{
+         System.out.print("enter numberator");
+         int num1= sc.nextInt();
+         System.out.print("enter denominator");
+         int num2= sc.nextInt();
+         int output=num1/num2;
+         System.out.println ("Result: "+output);
+      }
+      catch(ArithmeticException e){
+         System.out.println ("You Shouldn't divide a number by zero");
+      }
+      catch(InputMismatchException r){
+         System.out.println("you must enter integers only please try again");
+      }
+   }
+}
